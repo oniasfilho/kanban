01b29ebaa20ab088244b4ccc4d2fc2e35a910a69
oniasfilho <onias.filho@gmail.com>
2023-06-01 20:48:35 -0400

    continues working on navbar dropdown

--- src/Component/NavbarBoardDropdown.js
+++ src/Component/NavbarBoardDropdown.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavbarDropdownModal from './NavbarDropdownModal';
 import { GENERAL_DATA } from '../Data/data';
 
 const NavbarBoardDropdown = () => {
+	const [expanded, setExpanded] = useState(false);
+
+	const toggleExpanded = () => {
+
+		setExpanded(!expanded);
+	}
 
 	return (
-		<div className='nav-dropdown-wrapper'>
-			{GENERAL_DATA.boards[0].name}
-			<img className='nav-dropdown-down-arrow' src={process.env.PUBLIC_URL + '/assets/icon-chevron-down.svg'} alt="Dropdown Arrow" />
-		</div>
+		<>
+			<div className='nav-dropdown-wrapper' onClick={toggleExpanded}>
+				{GENERAL_DATA.boards[0].name}
+				<img
+					className='nav-dropdown-down-arrow'
+					src={process.env.PUBLIC_URL + `/assets/icon-chevron-${expanded ? 'up' : 'down'}.svg`}
+					alt="Dropdown Arrow" />
+			</div>
+
+			{expanded && (
+				<NavbarDropdownModal toggleExpanded={toggleExpanded} boards={GENERAL_DATA.boards} />
+			)}
+		</>
 	)
 }
 
---
+++ src/Component/NavbarDropdownModal.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const NavbarDropdownModal = ({ toggleExpanded, boards }) => {
+	const handleClick = (e) => {
+		if (e.target.closest('.navbar-modal') === null) {
+			toggleExpanded();
+		}
+	}
+	return (
+		<div className='navbar-modal-wrapper' onClick={handleClick}>
+			<div className='navbar-modal'>
+				<div className="navbar-modal-header">
+					all boards ({boards.length})
+				</div>
+				<div className="navbar-modal-content">
+					{boards.map((each, index) => (
+						<div key={each.name} className="navbar-modal-board-option">
+							<img
+								className='navbar-board-icon'
+								src={process.env.PUBLIC_URL + '/assets/icon-board.svg'}
+								alt="Board Icon" />
+							{each.name}
+						</div>
+					))}
+					<div className="navbar-modal-board-option create-new-board">
+						<img
+							className='navbar-board-icon new-board-icon'
+							src={process.env.PUBLIC_URL + '/assets/icon-board-dark-purple.svg'}
+							alt="Board Icon" />
+						<img
+							className='navbar-add-board-icon'
+							src={process.env.PUBLIC_URL + '/assets/icon-add-task-mobile-dark-purple.svg'}
+							alt="Add Icon" />
+						Create New Board
+					</div>
+				</div>
+				<div className="navbar-modal-footer">
+					light/dark mode toggle
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default NavbarDropdownModal
