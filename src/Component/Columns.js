4fe69934c40865f532d196a28a3f826760de1c16
oniasfilho <onias.filho@gmail.com>
2023-06-04 14:58:56 -0400

    creates multiple columns based on data file

--- src/Component/Columns.js
+++ src/Component/Columns.js
@@ -3,27 +3,30 @@ import { useSelector } from 'react-redux'
 
 const Columns = () => {
 	const { currentBoard } = useSelector(state => state.content)
-	const column = currentBoard.columns[0];
-	const { tasks } = column;
+	const { columns } = currentBoard;
+	// const column = currentBoard.columns[0];
+	// const { tasks } = column;
 	return (
 		<div className='columns-wrapper'>
-			<div className="single-column">
-				<div className="column-header-wrapper">
-					<div className="todo-circle" />{column.name} ( {column.tasks.length} )
+			{columns.map((column) => (
+				<div className="single-column">
+					<div className="column-header-wrapper">
+						<div className="todo-circle" />{column.name} ( {column.tasks.length} )
+					</div>
+					<div className="column-items">
+						{column.tasks.map((each) => (
+							<div key={each.title} className="column-item">
+								<p className="task-title">
+									{each.title}
+								</p>
+								<p className="subtasks-status">
+									{each.subtasks.reduce((count, subtask) => count + (subtask.isCompleted ? 1 : 0), 0)} of {each.subtasks.length} subtasks
+								</p>
+							</div>
+						))}
+					</div>
 				</div>
-				<div className="column-items">
-					{tasks.map((each) => (
-						<div key={each.title} className="column-item">
-							<p className="task-title">
-								{each.title}
-							</p>
-							<p className="subtasks-status">
-								{each.subtasks.reduce((count, subtask) => count + (subtask.isCompleted ? 1 : 0), 0)} of {each.subtasks.length} subtasks
-							</p>
-						</div>
-					))}
-				</div>
-			</div>
+			))}
 		</div>
 	)
 }
