40fcb570141368b56daee27215b38e0a9d60fe7a
oniasfilho <onias.filho@gmail.com>
2023-06-08 16:20:09 -0400

    fixes mobile layout

--- src/Component/NavbarBoardDropdown.js
+++ src/Component/NavbarBoardDropdown.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 
 const NavbarBoardDropdown = () => {
 	const [expanded, setExpanded] = useState(false);
-	// const name = useSelector(state => state.content.currentBoard || null)
+	const name = useSelector(state => state.content.currentBoard?.name)
 
 	const boards = useSelector(state => state.content.boards)
 	const toggleExpanded = () => {
@@ -15,7 +15,7 @@ const NavbarBoardDropdown = () => {
 	return (
 		<>
 			<div className='nav-dropdown-wrapper' onClick={toggleExpanded}>
-				{/* {name} */}
+				{name}
 				<img
 					className='nav-dropdown-down-arrow'
 					src={process.env.PUBLIC_URL + `/assets/icon-chevron-${expanded ? 'up' : 'down'}.svg`}
